0a906ca8a5029b3226d7621807a25b679d569536
Martin12312 <martinsklevs@gmail.com>
2017-08-15 14:13:41 +0300

    commit

--- app/src/main/java/com/rock/werool/piensunmaize/MainMenu.java
+++ app/src/main/java/com/rock/werool/piensunmaize/MainMenu.java
@@ -1,5 +1,6 @@
 package com.rock.werool.piensunmaize;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        //Intent intent = new Intent(this, BarcodeScanner.class);
+        //startActivity(intent);
+        startActivity(new Intent(MainMenu.this, BarcodeScanner.class));
     }
 }
