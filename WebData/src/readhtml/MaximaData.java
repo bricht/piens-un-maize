f6c5363a0b339c00fed47911ee7d1caa3672ebc2
Lauris Lazda <lauris.lazda@gmail.com>
2017-08-16 13:05:00 +0300

    succesfuly reading categories

--- WebData/src/readhtml/MaximaData.java
+++ WebData/src/readhtml/MaximaData.java
@@ -9,10 +9,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MaximaData {
+	// TODO: (?) boolean field to identify discounts
+	private BufferedReader in;
 
+	private List<String> urlCategory;
+	private List<String> urlList;
 	private List<String> products;
 	private List<Double> prices;
-	private List<String> urlList;
 
 	// temporary main method for testing
 	public static void main(String[] args) {
@@ -21,30 +24,32 @@ public class MaximaData {
 	}
 
 	public MaximaData() {
+		urlCategory = new ArrayList<>();
+		urlList = new ArrayList<>();
 		products = new ArrayList<>();
 		prices = new ArrayList<>();
-		urlList = new ArrayList<>();
 
-		collectURLs();
 		try {
-			collectData();
+			collectURLs();
+			// collectData();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	// TODO: sporta_uzturs has no subcategorys
+	// Add special support for it
 	private void collectData() throws Exception {
 		for (String string : urlList) {
 			// TODO: check if URL has multiple pages
 			URL url = new URL(string);
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					url.openStream()));
+			in = new BufferedReader(new InputStreamReader(url.openStream()));
 
 			String inputLine;
+			Matcher matcher;
 			Pattern namePattern = Pattern.compile("(?<=>)(.+?)(?=</a>)");
 			Pattern pricePattern = Pattern
 					.compile("(?<=<strong>)(\\d{1,3})(,)(\\d{2})(?= €</strong>)");
-			Matcher matcher;
 
 			while ((inputLine = in.readLine()) != null) {
 				boolean isTable = false;
@@ -95,14 +100,63 @@ public class MaximaData {
 		}
 	}
 
-	private void collectURLs() {
-		String urlPrefix = "https://www.e-maxima.lv/Produkti/partika_dzerieni/";
-		// TODO: get all categories from e-Maxima
-		String urlCategory = "augli_darzeni/";
-		// TODO: get all product pages from e-Maxima
-		String urlPage = "augli.aspx";
+	private void collectURLs() throws Exception {
+		String homeURL = "https://www.e-maxima.lv";
+		// Page to get all the categories and products
+		String indexPage = homeURL + "/Produkti/partika_dzerieni.aspx";
+		URL url = new URL(indexPage);
+		in = new BufferedReader(new InputStreamReader(url.openStream()));
 
-		urlList.add(urlPrefix + urlCategory + urlPage);
+		String inputLine;
+		Matcher matcher;
+		Pattern categoryPattern = Pattern
+				.compile("(?<=(<a href=\"/Produkti/partika_dzerieni))(/[^/]+?)(?=(\\.aspx\">.+?)</a>)");
+		// gets category URLs
+		while ((inputLine = in.readLine()) != null) {
+			// test
+			if ((inputLine.replaceAll("\\s", ""))
+					.equals("<spanid=\"ctl00_phSiteMapMenu_menuCategory_menuSitemap\">")) {
+				while ((inputLine = in.readLine()) != null) {
+					// TODO: (?) get product category names
+					// into a separate list/class field for database
+					if ((inputLine.replaceAll("\\s", "")).equals("</span>")) {
+						break;
+					}
+
+					matcher = categoryPattern.matcher(inputLine);
+					if (matcher.find()) {
+						urlCategory.add(matcher.group());
+						// temporary output for testing
+						System.out.println(matcher.group());
+					}
+				}
+				break;
+			}
+		}
+		in.close();
+
+		// TODO: for each category in urlCategory
+		Pattern urlPattern = Pattern.compile("");
+		in = new BufferedReader(new InputStreamReader(url.openStream()));
+		// gets product URLs (subcategories)
+		while ((inputLine = in.readLine()) != null) {
+			if ((inputLine.replaceAll("\\s", ""))
+					.equals("<spanid=\"ctl00_phSiteMapMenu_menuCategory_menuSitemap\">")) {
+				// TODO: (?) get product subcategory names
+				// into a separate list/class field for database
+				if ((inputLine.replaceAll("\\s", "")).equals("</span>")) {
+					break;
+				}
+
+				matcher = urlPattern.matcher(inputLine);
+				if (matcher.find()) {
+
+				}
+			}
+		}
+		in.close();
+
+		urlList.add("https://www.e-maxima.lv/Produkti/partika_dzerieni/augli_darzeni/augli.aspx");
 	}
 
 	// TODO: Method that returns collected data
