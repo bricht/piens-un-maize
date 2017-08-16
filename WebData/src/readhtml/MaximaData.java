7ca2e8458c88555e931b08f7d824eaee2d265ddd
Lauris Lazda <lauris.lazda@gmail.com>
2017-08-16 09:16:38 +0300

    working on getting all product URLs from e-maxima

--- WebData/src/readhtml/MaximaData.java
+++ WebData/src/readhtml/MaximaData.java
@@ -12,7 +12,7 @@ public class MaximaData {
 
 	private List<String> products;
 	private List<Double> prices;
-	private List<String> url;
+	private List<String> urlList;
 
 	// temporary main method for testing
 	public static void main(String[] args) {
@@ -23,7 +23,7 @@ public class MaximaData {
 	public MaximaData() {
 		products = new ArrayList<>();
 		prices = new ArrayList<>();
-		url = new ArrayList<>();
+		urlList = new ArrayList<>();
 
 		collectURLs();
 		try {
@@ -34,73 +34,81 @@ public class MaximaData {
 	}
 
 	private void collectData() throws Exception {
-		// TODO: for each URL in List url ...
-		// Test URL
-		URL url = new URL(
-				"https://www.e-maxima.lv/Produkti/partika_dzerieni/augli_darzeni/salati_garsaugi/svaigi_salati.aspx");
-		BufferedReader in = new BufferedReader(new InputStreamReader(
-				url.openStream()));
+		for (String string : urlList) {
+			// TODO: check if URL has multiple pages
+			URL url = new URL(string);
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					url.openStream()));
 
-		String inputLine;
-		Pattern namePattern = Pattern.compile("(?<=>)(.+?)(?=</a>)");
-		Pattern pricePattern = Pattern
-				.compile("(?<=<strong>)(\\d{1,3})(,)(\\d{2})(?= €</strong>)");
-		Matcher matcher;
+			String inputLine;
+			Pattern namePattern = Pattern.compile("(?<=>)(.+?)(?=</a>)");
+			Pattern pricePattern = Pattern
+					.compile("(?<=<strong>)(\\d{1,3})(,)(\\d{2})(?= €</strong>)");
+			Matcher matcher;
 
-		while ((inputLine = in.readLine()) != null) {
-			boolean isTable = false;
-			boolean isContent = false;
-			if ((inputLine.replaceAll("\\s", ""))
-					.equals("<divid=\"main-content\">")) {
-				while ((inputLine = in.readLine()) != null) {
-					if (isContent) {
+			while ((inputLine = in.readLine()) != null) {
+				boolean isTable = false;
+				boolean isContent = false;
+				if ((inputLine.replaceAll("\\s", ""))
+						.equals("<divid=\"main-content\">")) {
+					while ((inputLine = in.readLine()) != null) {
+						if (isContent) {
+							if ((inputLine.replaceAll("\\s", ""))
+									.equals("</tbody></table>")) {
+								break;
+							}
+							if ((inputLine.replaceAll("\\s", ""))
+									.equals("<h3>")) {
+								inputLine = in.readLine();
+								matcher = namePattern.matcher(inputLine);
+								matcher.find();
+								products.add(matcher.group());
+								// temporary output for testing
+								System.out.println(matcher.group());
+							}
+							if ((inputLine.replaceAll("\\s", ""))
+									.equals("<pclass=\"guide\">")) {
+								inputLine = in.readLine();
+								matcher = pricePattern.matcher(inputLine);
+								matcher.find();
+								prices.add(Double.parseDouble((matcher.group())
+										.replaceAll(",", ".")));
+								// temporary output for testing
+								System.out.println(matcher.group());
+							}
+						}
+						if (isTable) {
+							if ((inputLine.replaceAll("\\s", ""))
+									.equals("<tr>")) {
+								isContent = true;
+							}
+						}
 						if ((inputLine.replaceAll("\\s", ""))
-								.equals("</tbody></table>")) {
-							break;
-						}
-						if ((inputLine.replaceAll("\\s", "")).equals("<h3>")) {
-							inputLine = in.readLine();
-							matcher = namePattern.matcher(inputLine);
-							matcher.find();
-							products.add(matcher.group());
-							// temporary output for testing
-							System.out.println(matcher.group());
-						}
-						if ((inputLine.replaceAll("\\s", ""))
-								.equals("<pclass=\"guide\">")) {
-							inputLine = in.readLine();
-							matcher = pricePattern.matcher(inputLine);
-							matcher.find();
-							prices.add(Double.parseDouble((matcher.group())
-									.replaceAll(",", ".")));
-							// temporary output for testing
-							System.out.println(matcher.group());
+								.equals("<tableclass='datatype1'>")) {
+							isTable = true;
 						}
 					}
-					if (isTable) {
-						if ((inputLine.replaceAll("\\s", "")).equals("<tr>")) {
-							isContent = true;
-						}
-					}
-					if ((inputLine.replaceAll("\\s", ""))
-							.equals("<tableclass='datatype1'>")) {
-						isTable = true;
-					}
+					break;
 				}
-				break;
 			}
+			in.close();
 		}
-		in.close();
 	}
 
 	private void collectURLs() {
-		// TODO: get all the necessary URLs from e-Maxima
+		String urlPrefix = "https://www.e-maxima.lv/Produkti/partika_dzerieni/";
+		// TODO: get all categories from e-Maxima
+		String urlCategory = "augli_darzeni/";
+		// TODO: get all product pages from e-Maxima
+		String urlPage = "augli.aspx";
+
+		urlList.add(urlPrefix + urlCategory + urlPage);
 	}
 
 	// TODO: Method that returns collected data
 	// Return type to be determined (special class, list ...)
 	// private (returnType) getData () { }
-	
+
 	// https://stackoverflow.com/questions/14865283/proper-git-workflow-scheme-with-multiple-developers-working-on-same-task
 	// https://www.atlassian.com/git/tutorials/comparing-workflows#feature-branch-workflow
 	// https://stackoverflow.com/questions/4556467/git-pull-or-git-merge-between-master-and-development-branches
