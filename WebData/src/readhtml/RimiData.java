b8fbb0720d8a5001a649ba76cf9349afc3bb0afa
Janis Lazda <jlazda94@gmail.com>
2017-08-16 09:32:43 +0300

    Working on getting data from app.rimi.lv product page

--- WebData/src/readhtml/RimiData.java
+++ WebData/src/readhtml/RimiData.java
@@ -7,19 +7,32 @@ import java.net.URL;
 public class RimiData {
 
 	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		RimiData data = new RimiData();
+	}
+
+	public RimiData() {
+		collectURLs();
 		try {
-			@SuppressWarnings("unused")
-			RimiData data = new RimiData();
+			collectData();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public RimiData() throws Exception {
+	private void collectData() throws Exception {
 		// Test URL (tomatoes)
 		URL url = new URL("https://app.rimi.lv/products/1179");
 		BufferedReader in = new BufferedReader(new InputStreamReader(
 				url.openStream()));
 		in.close();
 	}
+
+	private void collectURLs() {
+		// TODO: get all the necessary URLs from app.rimi.lv
+	}
+
+	// TODO: Method that returns collected data
+	// Return type to be determined (special class, list ...)
+	// private (returnType) getData () { }
 }
