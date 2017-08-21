d58c6c0c7759c359b98e0fa92bf2a1b254bba32f
Roberts Staskevics <roberts.staskevics@gmail.com>
2017-08-21 14:25:07 +0300

    Updated ThankActivity, added auto closing and goto main

--- app/src/main/java/com/rock/werool/piensunmaize/add/ThankActivity.java
+++ app/src/main/java/com/rock/werool/piensunmaize/add/ThankActivity.java
@@ -1,20 +1,33 @@
 package com.rock.werool.piensunmaize.add;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.rock.werool.piensunmaize.R;
+import com.rock.werool.piensunmaize.mainpage.GreetingsActivity;
+import com.rock.werool.piensunmaize.mainpage.MainMenu;
 
 public class ThankActivity extends AppCompatActivity {
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thank);
 
+        handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                finish();
+                startActivity(new Intent(ThankActivity.this, MainMenu.class));
+
+            }
+        }, 1500);
 
     }
 }
