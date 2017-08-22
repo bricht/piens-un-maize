a92a6178f7fe430189b14eb4110f053bddec9a09
Martin12312 <martinsklevs@gmail.com>
2017-08-22 14:19:45 +0300

    Made the search activities use an 2d array instead of an object list

--- app/src/main/java/com/rock/werool/piensunmaize/search/by_product/SearchByProductActivity.java
+++ app/src/main/java/com/rock/werool/piensunmaize/search/by_product/SearchByProductActivity.java
@@ -31,18 +31,19 @@ public class SearchByProductActivity extends AppCompatActivity {              //
     MyCustomAdapter dataAdapter;
     ArrayList<Product> products = new ArrayList<>();
     ArrayList<Product> productSearchResults = new ArrayList<>();               //ListView uses productSearchResults instead of products!
+    String [][] array;
 
     @Override
     protected void onResume() {
         super.onResume();
-        registerReceiver(SearchProductList , new IntentFilter ("ProcessedQueryResult"));         //Registers BroadcastReceivers
+        //registerReceiver(SearchProductList , new IntentFilter ("ProcessedQueryResult"));         //Registers BroadcastReceivers
         registerReceiver(SearchProductSQL , new IntentFilter ("QUERY_RESULT"));
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-            unregisterReceiver(SearchProductList);         //Unregisters BroadcastReceivers
+            //unregisterReceiver(SearchProductList);         //Unregisters BroadcastReceivers
             unregisterReceiver(SearchProductSQL);
     }
 
@@ -63,57 +64,22 @@ public class SearchByProductActivity extends AppCompatActivity {              //
         intentForSQL.putExtra(SQLiteQuery.SRC_ADDRESS, (String) null);
         startService(intentForSQL);             //Starts SQLite intent service
         Log.v("BroadcastDebug", "SQLite query broadcast sent from SearchByProductActivity");
-        /*
-        products.add(new Product("Apple", "21"));           //TODO Implement local database query and format the data into ArrayList<Product>
-        products.add(new Product("Orange", "42"));
-        products.add(new Product("Apple", "21"));
-        products.add(new Product("Orange", "42"));
-        products.add(new Product("Apple", "21"));
-        products.add(new Product("Orange", "42"));
-        products.add(new Product("Apple", "21"));
-        products.add(new Product("Orange", "42"));
-        products.add(new Product("Apple", "21"));
-        products.add(new Product("Orange", "42"));
-        products.add(new Product("Apple", "21"));
-        products.add(new Product("Orange", "42"));
-        products.add(new Product("Apple", "21"));
-        products.add(new Product("Orange", "42"));
 
-        productSearchResults.addAll(products);                 //ListView initially shows all products
-        displayListView(productSearchResults);
-        */
         addSearchBarListener();
     }
-    private void displayListView(ArrayList<Product> inputList) {
-
+    private void displayListView(ArrayList<String> inputList) {
         dataAdapter = new MyCustomAdapter(this, R.layout.itemname_price, inputList);
         ListView listView = (ListView)findViewById(R.id.listviewsearchproduct);
         listView.setAdapter(dataAdapter);
-        /*
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(getApplicationContext(), SelectStoreActivity.class);
-                //dataAdapter.ViewHolder holderForClicked = dataAdapter.getItemAtPosition(position);
-                //View item = (View) adapterView.getItemAtPosition(i);
-                final String clickedProductName = dataAdapter.productList.get(i).getName();
-                final String clickedProductAveragePrice = dataAdapter.productList.get(i).getPrice();
-                intent.putExtra("clickedProductName", clickedProductName);      //Passes parameters to the activity
-                intent.putExtra("clickedProductAveragePrice", clickedProductAveragePrice);    //.putExtra(variableName, variableValue)
-                startActivity(intent);
-            }
-        });
-        */
     }
-    private class MyCustomAdapter extends ArrayAdapter<Product> {
+    private class MyCustomAdapter extends ArrayAdapter<String> {
 
-        private ArrayList<Product> productList;
+        private ArrayList<String> productList;
 
         public MyCustomAdapter(Context context, int textViewResourceId,
-                               ArrayList<Product> productList) {
+                               ArrayList<String> productList) {
             super(context, textViewResourceId, productList);
-            this.productList = new ArrayList<Product>();
-            this.productList.addAll(productList);
+            this.productList = productList;   //1D
         }
 
         private class ViewHolder {
@@ -139,9 +105,8 @@ public class SearchByProductActivity extends AppCompatActivity {              //
                 holder.averagePrice = (TextView) convertView.findViewById(R.id.searchProductPrice);
                 holder.productClckSpace = (Space) convertView.findViewById(R.id.searchProductClickSpace);
 
-                Product product = productList.get(position);
-                holder.name.setText(product.getName());
-                holder.averagePrice.setText(product.getPrice());
+                holder.name.setText(array[position][0]);
+                holder.averagePrice.setText(array[position][1]);
 
                 final String clickedProductName = holder.name.getText().toString();
                 final String clickedProductAveragePrice = holder.averagePrice.getText().toString();
@@ -151,7 +116,6 @@ public class SearchByProductActivity extends AppCompatActivity {              //
                     @Override
                     public void onClick(View view) {
                         Intent intent = new Intent(getApplicationContext(), SelectStoreActivity.class);
-                        //final String clickedProductName = holder.name.toString();
                         intent.putExtra("clickedProductName", clickedProductName);      //Passes parameters to the activity
                         intent.putExtra("clickedProductAveragePrice", clickedProductAveragePrice);    //.putExtra(variableName, variableValue)
                         startActivity(intent);
@@ -161,7 +125,6 @@ public class SearchByProductActivity extends AppCompatActivity {              //
                     @Override
                     public void onClick(View view) {
                         Intent intent = new Intent(getApplicationContext(), SelectStoreActivity.class);
-                        //final String clickedProductName = holder.name.toString();
                         intent.putExtra("clickedProductName", clickedProductName);      //Passes parameters to the activity
                         intent.putExtra("clickedProductAveragePrice", clickedProductAveragePrice);    //.putExtra(variableName, variableValue)
                         startActivity(intent);
@@ -186,9 +149,8 @@ public class SearchByProductActivity extends AppCompatActivity {              //
                 holder = (ViewHolder) convertView.getTag();                         //If row is already created then get the holder from it
             }
 
-            Product product = productList.get(position);
-            holder.name.setText(product.getName());
-            holder.averagePrice.setText(product.getPrice());
+            holder.name.setText(array[position][0]);
+            holder.averagePrice.setText(array[position][1]);
 
             return convertView;
         }
@@ -219,23 +181,23 @@ public class SearchByProductActivity extends AppCompatActivity {              //
             }
         });
     }
-    BroadcastReceiver SearchProductList = new BroadcastReceiver() {              //Receives an ArrayList from QueryProcessingIntentService
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            productSearchResults.clear();
-            productSearchResults.addAll((ArrayList<Product>) intent.getSerializableExtra("ArrayList<Product>"));
-            displayListView(productSearchResults);                 //TODO Maybe not a good way to update ListView
-        }
-
-    };
 
     BroadcastReceiver SearchProductSQL = new BroadcastReceiver() {              //Receives broadcast from SQLite database class
         @Override
         public void onReceive(Context context, Intent intent) {
             Intent intentForService = new Intent(getApplicationContext(), QueryProcessingIntentService.class);
-            intentForService.putExtra("String[][]", intent.getBundleExtra(SQLiteQuery.QUERY_RESULT));
-            intentForService.putExtra("currentQuery", "SEND_PRODUCTNAME_GET_PRODUCTNAME_AVERAGEPRICE");
-            getApplicationContext().startService(intentForService);
+            Bundle bun = intent.getBundleExtra(SQLiteQuery.QUERY_RESULT);
+            array = (String[][]) bun.getSerializable("String[][]");
+            //intentForService.putExtra("String[][]", array);
+            //intentForService.putExtra("currentQuery", "SEND_PRODUCTNAME_GET_PRODUCTNAME_AVERAGEPRICE");
+            ArrayList<String> array1D = new ArrayList<>();
+            if (array != null) {
+                //array1D = new String[array.length];
+                for (int i = 0; i < array.length; i++) {
+                    array1D.add(array[i][0]);
+                }
+            }
+            displayListView(array1D);
         }
     };
 }
--- app/src/main/java/com/rock/werool/piensunmaize/search/by_product/SelectStoreActivity.java
+++ app/src/main/java/com/rock/werool/piensunmaize/search/by_product/SelectStoreActivity.java
@@ -29,18 +29,17 @@ public class SelectStoreActivity extends AppCompatActivity {
     ArrayList<Store> stores = new ArrayList<>();
     ArrayList<Store> storeSearchResults = new ArrayList<>();            //ListView uses storeSearchResults instead of stores!
     String clickedProductName;
+    String [][] array;
 
     @Override
     protected void onResume() {
         super.onResume();
-        registerReceiver(SelectStoreList , new IntentFilter("ProcessedQueryResult"));         //Registers BroadcastReceivers
         registerReceiver(SelectStoreSQL , new IntentFilter ("QUERY_RESULT"));
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        unregisterReceiver(SelectStoreList);         //Unregisters BroadcastReceivers
         unregisterReceiver(SelectStoreSQL);
     }
 
@@ -53,7 +52,7 @@ public class SelectStoreActivity extends AppCompatActivity {
         TextView productNameTextView = (TextView) findViewById(R.id.selectedProductName);
         productNameTextView.setText(clickedProductName);
 
-        Intent intentForSQL = new Intent(getApplicationContext(), QueryProcessingIntentService.class);
+        Intent intentForSQL = new Intent(getApplicationContext(), SQLiteQuery.class);
         intentForSQL.putExtra(SQLiteQuery.SRC_TYPE, SQLiteQuery.SRC_PRICE);     //Price for specific product
         intentForSQL.putExtra(SQLiteQuery.SRC_NAME, clickedProductName);     //TODO may need to turn to lowercase
         intentForSQL.putExtra(SQLiteQuery.SRC_STORE, "");       //All stores
@@ -61,52 +60,28 @@ public class SelectStoreActivity extends AppCompatActivity {
         startService(intentForSQL);             //Starts SQLite intent service
         Log.v("BroadcastDebug", "SQLite query broadcast sent from SelectStoreActivity");
 
-        /*
-        stores.add(new Store("Rimi", "Somewhere 24"));          //TODO Implement local database query and format the data into ArrayList<Store>
-        stores.add(new Store("Ma\nxima", "Anywhere 42"));
-        stores.add(new Store("Rimi\nRimi", "Somewhere 24"));
-        stores.add(new Store("Maxima", "Anywhere 42"));
-        stores.add(new Store("Rimi", "Somewhere 24"));
-        stores.add(new Store("Maxima", "Anywhere 42"));
-        stores.add(new Store("Rimi", "Somewhere 24"));
-        stores.add(new Store("Maxima", "Anywhere 42"));
-        stores.add(new Store("Rimi", "Somewhere 24"));
-        stores.add(new Store("Maxima", "Anywhere 42"));
-        stores.add(new Store("Rimi", "Somewhere 24"));
-        stores.add(new Store("Maxima", "Anywhere 42"));
-        stores.add(new Store("Rimi", "Somewhere 24"));
-        stores.add(new Store("Maxima", "Anywhere 42"));
-        stores.add(new Store("Rimi", "Somewhere 24"));
-        stores.add(new Store("Maxima", "Anywhere 42"));
-        stores.add(new Store("Rimi", "Somewhere 24"));
-        stores.add(new Store("Maxima", "Anywhere 42"));
-
-        storeSearchResults.addAll(stores);                  //ListView initially shows all stores
-        displayListView(storeSearchResults);
-        */
         final EditText selectStoreName = (EditText)findViewById(R.id.selectStoreNameText);
         final EditText selectStoreAddress = (EditText)findViewById(R.id.selectStoreAddressText);
         addSearchBarListener(selectStoreName);
         addSearchBarListener(selectStoreAddress);    //Add listeners to both text fields
     }
-    private void displayListView(ArrayList<Store> inputList) {
+    private void displayListView(ArrayList<String> inputList) {
 
         dataAdapter = new MyCustomAdapter(this, R.layout.storename_address_itemprice, inputList);
         ListView listView = (ListView)findViewById(R.id.listviewselectstore);
         listView.setAdapter(dataAdapter);
     }
 
-    private class MyCustomAdapter extends ArrayAdapter<Store> {
+    private class MyCustomAdapter extends ArrayAdapter<String> {
         Context context;
         int textViewResourceId;
-        private ArrayList<Store> storeList;
+        private ArrayList<String> storeList;
 
         public MyCustomAdapter(Context context, int textViewResourceId,
-                               ArrayList<Store> storeList) {
+                               ArrayList<String> storeList) {
             super(context, textViewResourceId, storeList);
             this.textViewResourceId = textViewResourceId;
-            this.storeList = new ArrayList<Store>();
-            this.storeList.addAll(storeList);
+            this.storeList = storeList;
             this.context = context;
         }
         private class ViewHolder {
@@ -129,10 +104,9 @@ public class SelectStoreActivity extends AppCompatActivity {
                 holder.address = (TextView) convertView.findViewById(R.id.storeAddress);
                 holder.itemPrice = (TextView) convertView.findViewById(R.id.selectedProductPriceInStore);
 
-                Store store = storeList.get(position);
-                holder.name.setText(store.getName());
-                holder.address.setText(store.getAddress());
-                holder.itemPrice.setText(store.getPriceForProduct());
+                holder.name.setText(array[position][1]);
+                holder.address.setText(array[position][2]);
+                holder.itemPrice.setText(array[position][3]);
 
                 final String clickedStoreName = holder.name.getText().toString();
                 final String clickedStoreAddress = holder.address.getText().toString();
@@ -175,12 +149,9 @@ public class SelectStoreActivity extends AppCompatActivity {
                 holder = (MyCustomAdapter.ViewHolder) convertView.getTag();                     //If row is already created then get the holder from it
             }
 
-            Store store = storeList.get(position);
-            //holder.check.setChecked(store.getChecked());                      //Ignore this
-            //holder.check.setTag(store);
-            holder.name.setText(store.getName());
-            holder.address.setText(store.getAddress());
-            holder.itemPrice.setText(store.getPriceForProduct());
+            holder.name.setText(array[position][1]);
+            holder.address.setText(array[position][2]);
+            holder.itemPrice.setText(array[position][3]);
 
             return convertView;
         }
@@ -212,15 +183,6 @@ public class SelectStoreActivity extends AppCompatActivity {
             }
         });
     }
-    BroadcastReceiver SelectStoreList = new BroadcastReceiver() {              //Receives an ArrayList from QueryProcessingIntentService
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            storeSearchResults.clear();
-            storeSearchResults.addAll((ArrayList<Store>) intent.getSerializableExtra("ArrayList<Store>"));
-            displayListView(storeSearchResults);                 //TODO Maybe not a good way to update ListView
-        }
-
-    };
 
     BroadcastReceiver SelectStoreSQL = new BroadcastReceiver() {              //Receives broadcast from SQLite database class
         @Override
@@ -228,7 +190,16 @@ public class SelectStoreActivity extends AppCompatActivity {
             Intent intentForService = new Intent();
             intentForService.putExtra("Cursor", "PLACEHOLDER");     //TODO use real cursor
             intentForService.putExtra("currentQuery", "SEND_PRODUCTNAME_STORENAME_STOREADDRESS_GET_STORENAME_STOREADDRESS_PRODUCTPRICE");
-            getApplicationContext().startService(intent);
+            Bundle bun = intent.getBundleExtra(SQLiteQuery.QUERY_RESULT);
+            array = (String[][]) bun.getSerializable("String[][]");
+            ArrayList<String> array1D = new ArrayList<>();
+            if (array != null) {
+                //array1D = new String[array.length];
+                for (int i = 0; i < array.length; i++) {
+                    array1D.add(array[i][0]);
+                }
+            }
+            displayListView(array1D);
         }
     };
 }
