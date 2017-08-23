77627c8c70a51db598314568aed6167b307463be
Guntars <gunttars@gmail.com>
2017-08-23 19:21:03 +0300

    added funciotn to remoteDatabase addStoreByNameANdLocation()

--- app/src/main/java/com/rock/werool/piensunmaize/remoteDatabase/RemoteDatabase.java
+++ app/src/main/java/com/rock/werool/piensunmaize/remoteDatabase/RemoteDatabase.java
@@ -45,12 +45,14 @@ public class RemoteDatabase {
     private static final String ACTION_FIND_PRODUCT_BY_NAME = "findProductByName.php";
     private static final String ACTION_FIND_PRODUCT_BY_CATEGORY = "findProductByCategory.php";
     private static final String ACTION_FIND_PORDUCT_BY_STRING_KEY = "findProductByStringKey.php";
+    private static final String ACTION_FIND_PRODUCTS_BY_STORE = "findProductsByStore.php";
     private static final String ACTION_FIND_PORDUCT_BY_ID = "findProductByID.php";
 
 
     private static final String ACTION_FIND_STORE_BY_NAME = "findStoreByName";
     private static final String ACTION_FIND_STORE_BY_LOCATION = "findStoreByLocation";
     private static final String ACTION_FIND_STORE_BY_STRING_KEY = "findStoreByStringKey.php";
+    private static final String ACTION_FIND_STORE_BY_NAME_AND_LOCATION = "findStoreByNameAndLocation.php";
     private static final String ACTION_FIND_STORE_BY_ID = "findStoreByID.php";
 
     private static final String ACTION_GET_ALL_PRODUCTS = "getAllProducts.php";
@@ -165,7 +167,18 @@ public class RemoteDatabase {
 
 
 
+
+
     // FIND PRODUCTS .........
+    public void FindProductsByStore(Store store, String productKey, IDatabaseResponseHandler<Product> responseHandler) {
+        String requestUrl = this.removeWhiteSpaceFromUrl(this.url + ACTION_FIND_PRODUCTS_BY_STORE +
+                "?" + Store.TAG_ID + "=" + Product.TAG_NAME  + "=" + productKey);
+        StringRequest strRequest =
+                new StringRequest(Request.Method.GET, requestUrl,
+                        new OnProduct(responseHandler), new OnError(responseHandler));
+        this.ExecuteStringRequest(strRequest);
+    }
+
     public void FindProductByID(ArrayList<Integer> ids, IDatabaseResponseHandler<Product> responseHandler) {
         String data = "";
 
@@ -221,6 +234,17 @@ public class RemoteDatabase {
     public void FindStoreByLocation(String location, IDatabaseResponseHandler<Store> responseHandler) {
         FindStoreByStringKey(ACTION_FIND_STORE_BY_LOCATION, Store.TAG_LOCATION, location, responseHandler );
         this.lastStoreHandler = responseHandler;
+    }
+
+    public void FindStoreByNameAndLocation(String name, String location, IDatabaseResponseHandler<Store> responseHandler) {
+        String requestUrl = this.url + ACTION_FIND_STORE_BY_NAME_AND_LOCATION + "?" +
+                Store.TAG_NAME + "=" + name + "&" +
+                Store.TAG_LOCATION + "=" + location;
+        Log.d("requestUrl", requestUrl);
+        StringRequest strRequest =
+                new StringRequest(Request.Method.GET, requestUrl,
+                        new OnStore(responseHandler), new OnError(responseHandler));
+        this.ExecuteStringRequest(strRequest);
     }
     //Server side done
     public void FindStoreByStringKey(String key, IDatabaseResponseHandler<Store> responseHandler) {
